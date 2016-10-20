ca5d669734b40b9a668aafc24ca3ed5c34a6f525
Medallyon <chopper98@hotmail.de>
2016-10-20 17:13:37 +0100

    Updated some files and created a console.log() on "ready".

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import external modules necessary for this project
 var Discord = require("Discord.js")
-, colors = require("colors")
+, colors = require("colors/safe")
 , fs = require("fs-extra")
 , wikijs = require("wikijs");
 
@@ -9,5 +9,19 @@ var config = require("./config.json");
 
 // initiate a new client from the Discord.js library
 var client = new Discord.Client({ forceFetchUsers: true });
+
+// set the theme for the console to make it less confusing
+colors.setTheme({
+    log: 'cyan',
+    output: 'green',
+    info: 'yellow',
+    warn: 'red',
+    error: 'red'
+});
+
+client.on("ready", function()
+{
+    console.log(colors.log("Logged in and ready to go!\nCurrently serving " + client.guilds.size + " guilds and " + client.users.size + " users."));          
+})
 
 client.login(config.token);
