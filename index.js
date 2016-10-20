c8a685c800a9a40b936c84a2c94f10526bab6823
Medallyon <chopper98@hotmail.de>
2016-10-20 17:38:42 +0100

    Added comments.

--- index.js
+++ index.js
@@ -21,14 +21,17 @@ colors.setTheme({
 
 client.on("ready", function()
 {
+    // the bot is logged in - time to console.log()
     console.log(colors.log("Logged in and ready to go!\nCurrently serving " + client.guilds.size + " guilds and " + client.users.size + " users."));
 })
 
 client.on("message", function(msg)
 {
+    // define and then console.log() basic output based on if the message was sent on a public guild or in a private channel
     let output = (msg.guild ? `${new Date()}\n@${msg.author.username}: "${msg.content}"\n${msg.guild.name} #${msg.channel.name}` : `${new Date()}\n@${msg.author}: "${msg.content}"\nDM: #${msg.channel.recipient}`);
     console.log(colors.log("\n" + output));
 
+    // stop here - we don't want the bot to respond to itself!
     if (msg.author.id === client.user.id) return;
 })
 
