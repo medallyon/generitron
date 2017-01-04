ebda9d86b20308078daafbcc077f50b7399c7d3c
Tilman Raendchen <chopper98@hotmail.de>
2017-01-04 15:14:13 +0000

    Ignore days 10-20

--- utils/getFullDay.js
+++ utils/getFullDay.js
@@ -1,18 +1,20 @@
 ﻿var getFullDay = (day) => {
     day = String(day);
-    if (day.charAt(day.length - 1) == "1")
-        return day + "st";
-    else
+    if (!(day > 10 && day < 21)) {
+        if (day.charAt(day.length - 1) == "1")
+            return day + "st";
+        else
 
-    if (day.charAt(day.length - 1) == "2")
-        return day + "nd";
-    else
+        if (day.charAt(day.length - 1) == "2")
+            return day + "nd";
+        else
 
-    if (day.charAt(day.length - 1) == "3")
-        return day + "rd";
+        if (day.charAt(day.length - 1) == "3")
+            return day + "rd";
 
-    else
-        return day + "th";
+        else
+            return day + "th";
+    }
 }
 
 module.exports = getFullDay;
