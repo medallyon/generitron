2cc899085512acf1e2eef61cdcd83953588cb0c6
Tilman Raendchen <chopper98@hotmail.de>
2017-01-07 03:46:52 +0000

    Clean up and add to the comments

--- app.js
+++ app.js
@@ -11,20 +11,25 @@ var Discord = require("discord.js")
 // instantiate a new global Discord Client
 global.client = new Discord.Client();
 
-// imports from local directories
+// imports from local directories, globally available through client
 client["config"] = require(join(__dirname, "config.json"))
 , client["commands"] = require(join(__dirname, "commands.json"));
 
+// declare a variable indicating the CWD, simply for convenience
 global.__base = __dirname;
 
+// function to synchronously import .js modules
 var getModsSync = function(dir) {
+	// initiate imports and read the module dir
     let imports = {}
     , moduleFiles = fs.readdirSync(dir);
 
+    // for every file in the module dir, add it to imports
     for (let file of moduleFiles) {
         imports[file.replace(/\.(js)/g, "")] = require(join(dir, file));
     }
 
+    // return the whole imports object
     return imports;
 }
 
@@ -32,8 +37,6 @@ var getModsSync = function(dir) {
 global.modules = getModsSync(join(__dirname, "modules"));
 // dynamically import custom utilities
 global.utils = getModsSync(join(__dirname, "utils"));
-
-// console.log(modules);
 
 // =========================================================== //
 // ======================= [ Express ] ======================= //
@@ -52,16 +55,19 @@ app.use(require(join(__dirname, "routers", "index.js")));
 // ================================================================ //
 // ======================= [ Discord Login ] ======================= //
 
+// initiate a global handlers Collection
 global.handlers = new Discord.Collection();
 // login to Discord using the token found inside the config
 client.login(client.config.discord.loginToken)
     .then(() => {
-        console.log("logged in!");
+    	// once logged in, start the interactive web-panel
         app.listen(8080);
 
+        // read the directory for the handlers
         fs.readdir(join(__dirname, "handlers"), (err, files) => {
             if (err) console.error(err);
 
+            // import each of the handlers into the cache
             for (let handler of files) {
                 if (/.*\.(js)/g.test(handler)) handlers.set(handler.replace(/\.(js)/g, ""), require(join(__dirname, "handlers", handler)));
             }
