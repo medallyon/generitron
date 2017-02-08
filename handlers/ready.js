aaeeaad96d8c6442e59ce84c99d2504c0cc640ec
Tilman Raendchen <chopper98@hotmail.de>
2017-02-08 11:12:28 +0000

    make user count more accurate

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,5 +1,5 @@
 // "ready" event, taking no parameter
 client.once("ready", function () {
     // log a formatted string to the console to signify that the script is ready
-    console.log(`${client.user.username.toUpperCase()} is ready to serve ${client.guilds.size} guild${client.guilds.size == 1 ? "" : "s"}, accumulating ${client.users.size} user${client.users.size == 1 ? "" : "s"}.`);
+    console.log(`${client.user.username.toUpperCase()} is ready to serve ${client.guilds.size} guild${client.guilds.size == 1 ? "" : "s"}, accumulating ${client.users.filter(u => !u.bot).size} user${client.users.size == 1 ? "" : "s"}.`);
 });
