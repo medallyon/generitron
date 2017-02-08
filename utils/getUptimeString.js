d5efaf6e458ba5efb25bbdd36526bbd4b20b780e
Tilman Raendchen <chopper98@hotmail.de>
2017-02-08 15:00:12 +0000

    use fixed numbers instead of floats

--- utils/getUptimeString.js
+++ utils/getUptimeString.js
@@ -1,25 +1,25 @@
 function getUptimeString(uptime)
 {
-    let months = uptime / 30.436875 / 24 / 60 / 60 / 1000
-    , days = uptime / 24 / 60 / 60 / 1000
-    , hours = uptime / 60 / 60 / 1000
-    , minutes = uptime / 60 / 1000
-    , seconds = uptime / 1000
+    let months = (uptime / 30.436875 / 24 / 60 / 60 / 1000).toFixed()
+    , days = (uptime / 24 / 60 / 60 / 1000).toFixed()
+    , hours = (uptime / 60 / 60 / 1000).toFixed()
+    , minutes = (uptime / 60 / 1000).toFixed()
+    , seconds = (uptime / 1000).toFixed()
     , output = "";
 
-    if (months > 1) {
+    if (months >= 1) {
         output = `${months} Months, ${days} Days, ${hours} Hours, ${minutes} Minutes and ${seconds} Seconds.`;
     } else
-    if (days > 1) {
+    if (days >= 1) {
         output = `${days} Days, ${hours} Hours, ${minutes} Minutes and ${seconds} Seconds.`;
     } else
-    if (hours > 1) {
+    if (hours >= 1) {
         output = `${hours} Hours, ${minutes} Minutes and ${seconds} Seconds.`;
     } else
-    if (minutes > 1) {
+    if (minutes >= 1) {
         output = `${minutes} Minutes and ${seconds} Seconds.`;
     } else
-    if (seconds > 1) {
+    if (seconds >= 1) {
         output = `${seconds} Seconds.`;
     }
 
