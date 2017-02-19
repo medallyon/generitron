b302c7fc5ac20fcf9911aad46f48cffc219932c1
Tilman Raendchen <chopper98@hotmail.de>
2017-02-19 20:03:41 +0000

    update router to allow access to more resources

--- routers/resources.js
+++ routers/resources.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 var router = express.Router();
 
-router.get("/i/*.(css|js)", function(req, res)
+router.get("/i/*.(css|js|ico|png|jpg|svg|ttf|woff|woff2)", function(req, res)
 {
     res.sendFile(path.join(__base, "wwwroot", "resources", req.originalUrl).replace(/(\\i\\|\/i\/)/, "/"));
 });
